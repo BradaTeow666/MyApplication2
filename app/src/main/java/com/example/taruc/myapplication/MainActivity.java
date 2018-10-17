1bf10705784cbd9e83347a0d16bbce4ff37c4446
BradaTeow666 <teowtk-wa16@student.tarc.edu.my>
2018-10-17 17:51:14 +0800

    Initial

---
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.taruc.myapplication;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewMessage; //TextView = class
+    // textViewMessage = instance
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //onCreate = main function
+        super.onCreate(savedInstanceState);
+        //Show me the UI
+        //R = resource
+        //layout = folder
+        setContentView(R.layout.activity_main);
+
+        textViewMessage = findViewById(R.id.textViewMessage);
+
+    }
+
+    public void showMessage(View view){
+        textViewMessage.setText("Hello Brada");
+    }
+}
